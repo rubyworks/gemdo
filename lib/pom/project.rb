d45f07fa29f7abf3f2ac9a58a29651bc4fee30c6
trans <transfire@gmail.com>
2009-09-06 11:24:59 -0400

    root is located by meta/

--- lib/pom/project.rb
+++ lib/pom/project.rb
@@ -1,66 +1,45 @@
 require 'facets/pathname'
+require 'facets/string/op_div'
+
 require 'pom/metadata'
 require 'pom/project/manifest'
 require 'pom/project/history'
 
 module POM
 
-  # The Project class provides the location of specific
-  # directories and files in the project, plus access
-  # to the projects metadata, etc.
+  # The Project class provides the location of specific directories
+  # and files in the project, plus access to the projects metadata, etc.
   #
   class Project
 
     #
     LIB_DIRECTORY   = Pathname.new(File.dirname(File.dirname(__FILE__)))
 
-    #
-    ROOT_INDICATORS = ['{.,_}root', '{meta/,metadata*}']
+    # Root directory is indicated by the presence of a +meta/+ directory,
+    # or +.meta/+ hidden directory.
+    ROOT_INDICATORS = [ '{.meta,meta}/' ]
 
     #
-    README = "readme{,.txt}"
+    README = "readme{,.*}"
+
+# IDEA: A lazy pathname that contains a glob, and reduces to real when used.
 
     #
-    def initialize
-      @root   = locate_root || raise("can't locate project root")
-
-      # DEPRECATE, or use for ?
-      @admin  = root  #.glob_first('{,_}admin', :casefold) || root
-
+    def initialize(local=nil)
+      @root   = locate_root(local) || raise("can't locate project root")
+      @source = root
       @cache  = root + '.cache'
+      @task   = root + 'task'
+      @script = root + 'script'
+      @log    = root + 'log'
+      @doc    = root + 'doc'
 
-      if (root + '.config').directory?
-        @config = root + '.config'
-      else
-        @config = root + 'config'
-      end
+      @config = root.glob_first('{.,}config') || root + '.config'
+      #@plug  = root.glob_first('plug{,in,ins'}) || root + 'plug'
+      #@pack  = root.glob_first('{pack,pkg}{,s}') || root + 'pack'
 
-      @task  = root + "task"
-
-      #@build = cache + 'build'
-
-      #@log     = root + 'log'
-      #@pkg    = root.glob_first('pkg') || cache + 'pkg'
-
-      #else
-      #  @config  = root.glob_first('config') || admin + 'config'
-      #  @local   = root.glob_first('.local') || admin + 'local'
-      #  @build   = root.glob_first('build')  || admin + 'build'
-      #  @log    = root.glob_first('log')    || admin + 'log'
-      #  @pkg     = root.glob_first('{pack,packs,pkg}')  ||
-      #             admin.glob_first('{pack,packs,pkg}') ||
-      #             admin + 'pack'
-      #end
-
-      #@config_reap = config + 'reap'
-
-      @source  = root
-
-      # TODO: Not sure about this location. Where should plugins go?
-      @plugin = root + 'plugins/reap'
-
-      #@tmp    = Pathname.new(File.join(Dir.tmpdir, 'reap'))
-      #@tmp     = @cache + 'tmp'
+      #@tmp   = Pathname.new(File.join(Dir.tmpdir, 'reap'))
+      #@tmp   = cache + 'tmp'
     end
 
     # Metadata provides all the general information about the project.
@@ -78,7 +57,7 @@ module POM
     end
 
     # Project manifest file.
-    # TODO: Depreacate in favor of using manifest.file
+    # TODO: Deprecate in favor of using manifest.file ?
     def manifest_file
       root.glob_first('manifest{,.txt}', :casefold)
     end
@@ -87,69 +66,133 @@ module POM
     # up from the current working directory in search of the ROOT_INDICATOR.
     attr :root
 
-    # Project's admin directory. This is the same as root. It used to be
-    # different, but that use is being deprecated.
-    # TODO: DEPRECATE admin directory.
-    attr :admin
-
-    # Log directory.
-    def log
-      @log ||=(
-        dir = root.glob_first('{log,doc/log}{,s}') || root + 'doc/log'
-        dir.mkdir_p unless dir.exist?
-        dir
-      )
-    end
-
-    # Package directory.
-    def pack
-      @pkg ||=(
-        dir = root.glob_first('{pack,pkg}{,s}') || cache + 'pkg'
-        dir.mkdir_p unless dir.exist?
-        dir
-      )
-    end
-    alias_method :pkg, :pack
-
-    # Temporary directory.
-    def tmp
-      @tmp ||=(
-        dir = root.glob_first('tmp') || cache + 'tmp'
-        dir.mkdir_p unless dir.exist?
-        dir
-      )
-    end
-    alias_method :tmpdir, :tmp
-
-    # Configuration directory.
-    attr :config
-
-    # Task directory.
-    attr :task
-
-    # Config '.config/reap'
-    #attr :config_reap
-
-    # Cache directory.
-    attr :cache
-
-    # Plugin directory.
-    attr :plugin
-
     # Location of project source code. Currently, this is always
     # the same as the root.
     #
     # TODO: Support alternate source location in the future (?)
     attr :source
 
-    # Reap services configuration folder.
-    #attr :reap_services_folder
+    # Get pathname of given log +path+. Or without +path+
+    # returns the pathname for the log directory.
+    def log(path=nil)
+      if path
+        root.glob_first('log' / path)
+      else
+        @log ||= (root + 'log')
+      end
+      #@log ||=(
+      #  dir = root.glob_first('{log,doc/log}{,s}') || root + 'doc/log'
+      #  dir.mkdir_p unless dir.exist?
+      #  dir
+      #)
+    end
 
-    # Reap services configuration file.
-    #attr :reap_services_file
+    # Get pathname of given doc +path+. Or without +path+
+    # returns the pathname for the doc directory.
+    def doc(path=nil)
+      if path
+        root.glob_first('doc' / path)
+      else
+        @doc ||= (root + 'doc')
+      end
+    end
+
+    # Get pathname of given plugin +path+. Or without +path+
+    # returns the pathname for the package directory.
+    def pack(path=nil)
+      if path
+        root.glob_first('{pack,pkg}{,s}' / path)
+      else
+        @pack ||= (root.glob_first('{pack,pkg}{,s}') || (root + 'pack'))
+      end
+      #@pkg ||=(
+      #  dir = root.glob_first('{pack,pkg}{,s}') || 'pack'
+      #  dir.mkdir_p unless dir.exist?
+      #  dir
+      #)
+    end
+
+    # Alias for #pack.
+    alias_method :pkg, :pack
+
+    # Get pathname of given cache +path+. Or without +path+
+    # returns the pathname for the cache directory.
+    def cache(path=nil)
+      if path
+        root.glob_first('.cache' / path)
+      else
+        @cache ||= (root + '.cache')
+      end
+    end
+
+    # Get pathname of given plugin +path+. Or without +path+
+    # returns the pathname for the plugin directory.
+    def plugin(path=nil)
+      if path
+        root.glob_first('plug{,in,ins}' / path)
+      else
+        @plug ||= (root.glob_first('plug{,in,ins}') || (root + 'plug'))
+      end
+    end
+
+    # Alias for #plugin.
+    alias_method :plug, :plugin
+
+    # Get pathname of given script +path+. Or without +path+
+    # returns the pathname for the script directory.
+    def script(path=nil)
+      if path
+        root.glob_first('script' / path)
+      else
+        @script ||= (root + 'script')
+      end
+    end
+
+    # Get pathname of given task +path+. Or without +path+
+    # returns the pathname for the task directory.
+    #
+    # TODO: Are task and script different names for the same thing?
+    #
+    def task(path=nil)
+      if path
+        root.glob_first('task' / path)
+      else
+        @task ||= (root + 'task')
+      end
+    end
+
+    # Get pathname of given config +path+. Or without +path+
+    # Returns the path to the config directory (either +.config+
+    # or +config+).
+    def config(path=nil)
+      if path
+        root.glob_first('{.,}config' / path)
+      else
+        @config ||= (root.glob_first('{.,}config') || (root + '.config'))
+      end
+    end
+
+    # Get pathname of given temporary +path+. Or without +path+
+    # returns the pathname to the temporary directory.
+    #
+    # TODO: Use Dir.tmpdir instead ?
+    def tmp(path=nil)
+      if path
+        cache.glob_first('tmp' / path)
+      else
+        @tmp ||= (cache + 'tmp')
+      end
+      #@tmp ||=(
+      #  dir = root.glob_first('tmp') || cache + 'tmp'
+      #  dir.mkdir_p unless dir.exist?
+      #  dir
+      #)
+    end
+
+    # Alias for #tmp.
+    alias_method :tmpdir, :tmp
 
     # About project notice.
-    #
     def about(parts) #(*parts)
       if parts.empty?
         puts
@@ -212,12 +255,14 @@ module POM
       # by ascending up the directory tree from the current position
       # until the ROOT_INDICATOR is matched. Returns +nil+ if not found.
       #
-      def locate_root
-        dir = nil
-        ROOT_INDICATORS.find do |i|
-          dir = locate_root_at(i)
+      def locate_root(local=nil)
+        Dir.chdir(local||Dir.pwd) do
+          dir = nil
+          ROOT_INDICATORS.find do |i|
+            dir = locate_root_at(i)
+          end
+          dir ? Pathname.new(File.dirname(dir)) : nil
         end
-        return dir
       end
 
       #
@@ -239,5 +284,5 @@ module POM
 
   end#class Project
 
-end#module Reap
+end#module POM
 
