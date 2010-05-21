87f331a88fc442f62ea15f425e289f3c7a034de3
7rans <transfire@gmail.com>
2010-05-21 09:57:16 -0400

    new package file stores dependency metadata

--- lib/pom/package.rb
+++ lib/pom/package.rb
@@ -1,67 +1,179 @@
-require 'pom/yamlstore'
+require 'pom/core_ext/pathname'
 
 module POM
 
-  # Use +Reqfile+.
-  class Requirements < YAMLStore #class Requisites < MetaStore
+  #
+  class Package #PkgConfig
+    include Enumerable
+
+    # TODO: Narrow naming.
+    def self.filename
+      ['PACKAGE', 'Package', '.package', 'Pkgfile', '.pkgfile']
+    end
+
+    def self.find(root)
+      pattern = '{' + filename.join(',') + '}{,.yml,.yaml}'
+      root.glob(pattern).first
+    end
 
     #
-    def self.filename
-      ['Reqfile']
-    end
+    attr :dependencies
 
     #
     def initialize(root)
-      @data = {}
-      super(root)
+      @root = Pathname.new(root)
+      @file = self.class.find(@root)
+
+      @dependencies  = []
+
+      if @file && @file.exist?
+        parse(@file)
+      end
     end
 
     #
-    def method_missing(sym, *args)
-      meth = sym.to_s
-      name = meth.chomp('=')
-      case meth
-      when /=$/
-        @data[name] = Needs.new(*args)
-      else
-        if block_given? or args.size > 0
-          return super(sym, *args)
+    def each(&block)
+      @dependencies.each(&block)
+    end
+
+    def size
+      @dependencies.size
+    end
+
+    # List of platforms.
+    def platforms
+      map{ |dep| dep.platform }.flatten
+    end
+
+    # List of engines.
+    def engines
+      map{ |dep| dep.engine }.flatten
+    end
+
+    # Returns a mapping of dependencies cross-indexed by engine and platform.
+    def renditions
+      deps = {}
+      each do |dep|
+        deps[[dep.engine,dep.platform]] ||= []
+        deps[[dep.engine,dep.platform]] << self
+      end
+      deps
+    end
+
+    # Parse dependency configuration file.
+    def parse(file)
+      data  = YAML.load(File.new(file))
+      share = {}
+      data.each do |entry|
+        if String===entry
+          entry = {'package'=> entry}
+        end
+        if entry['package']
+          @dependencies << Dependency.new(share.merge(entry))
         else
-          return @data[name]
+          share.merge!(entry)
         end
       end
     end
 
-    # TODO: Good enough?
-    def to_yaml
-      @data.to_yaml
+  end
+
+  #
+  class Dependency
+
+    attr_accessor :name
+
+    attr_accessor :version
+
+    attr_accessor :group
+
+    attr_accessor :optional
+
+    attr_accessor :source
+
+    attr_accessor :engine
+
+    attr_accessor :platform
+
+    attr_accessor :path
+
+    #
+    def initialize(settings)
+      settings.each do |k,v|
+        __send__("#{k}=",v)
+      end
+    end
+
+    #
+    def package=(package)
+      parts = package.split(/\s+/)
+      @name    = parts.shift
+      @version = parts.join(' ')
+    end
+
+    #
+    def source=(source_list)
+      @source = list(source_list)
+    end
+
+    #
+    def group=(group_list)
+      @group = list(group_list)
+    end
+
+    alias_method :groups, :group
+    alias_method :groups=, :group=
+
+    #
+    def inspect
+      "#{name} #{version}".strip
+    end
+ 
+    #
+    def optional?
+      @optional
+    end
+
+    #
+    def development?
+      group.include?('dev') || group.include?('development')
+    end
+
+  protected
+
+    def list(value)
+      case value
+      when String
+        value.split(/\s+/)
+      else
+        value
+      end
     end
 
   end
 
-  # Needs are various types of dependencies.
-  class Needs
+=begin
+  # Specification for various types of dependencies.
+  class Requests
+
+    # Versions of Ruby supported/tested.
+    attr_accessor :ruby
+
+    # Platforms supported/tested.
+    attr_accessor :platforms
 
     # What other packages *must* this package have in order to function.
     # This includes any requirements neccessary for installation.
     attr_accessor :requires
 
-    # External requirements, outside of the normal packaging system.
-    attr_accessor :externals
-
     # What other packages *should* be used with this package.
     attr_accessor :recommend
 
     # What other packages *could* be useful with this package.
-    attr_accessor :suggest
+    attr_accessor :suggest #:optional?
 
     # With what other packages does this package conflict.
     attr_accessor :conflicts
-
-    # What other packages does this package replace. This is very much
-    # like #provides but expresses a closser relation. For instance
-    # "libXML" has been replaced by "libXML2".
-    attr_accessor :replaces
 
     # What other package(s) does this package provide the same dependency
     # fulfilment. For example, a package 'bar-plus' might fulfill the same
@@ -69,14 +181,19 @@ module POM
     # provide 'bar'.
     attr_accessor :provides
 
-    # Abirtary point list, especially about what might be needed
-    # to use or build or use this package that does not fit under
-    # +requires+. This is strictly information for the end-user
-    # to consider, eg. "Needs gcc 4.4+" or "Needs fast graphics card".
-    attr_accessor :consider
+    # What other packages does this package replace. This is very much
+    # like #provides but expresses a overriding relation. For instance
+    # "libXML" has been replaced by "libXML2".
+    attr_accessor :replaces
 
-    # Versions of Ruby supported/tested.
-    attr_accessor :ruby
+    # External requirements, outside of the normal packaging system.
+    attr_accessor :externals
+
+    ## Abirtary point list, especially about what might be needed
+    ## to use or build or use this package that does not fit under
+    ## package category. This is strictly information for the end-user
+    ## to consider, eg. "fast graphics card".
+    #attr_accessor :consider
 
     #
     def initialize(data={})
@@ -88,17 +205,33 @@ module POM
 
     #
     def initialize_defaults
+      @ruby      = []
+      @platforms = ['all']
       @requires  = []
-      @externals = []
       @recommend = []
       @suggest   = []
       @conflicts = []
       @replaces  = []
       @provides  = []
-      @consider  = []
+      @externals = []
+      #@consider  = []
+    end
+
+    #
+    def merge(other)
+      req = Requests.new
+      req.requires  = requires  + other.requires
+      req.recommend = recommend + other.recommend
+      req.suggest   = suggest   + other.suggest
+      req.conflicts = conflicts + other.conflicts
+      req.replaces  = replaces  + other.replaces
+      req.provides  = provides  + other.provides
+      req.externals = externals + other.externals
+      req
     end
 
   end
+=end
 
 end
 
