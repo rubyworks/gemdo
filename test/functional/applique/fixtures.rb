169ea7fd7eecda883aea40fc26504deade622fc5
7rans <transfire@gmail.com>
2010-06-17 15:45:29 -0400

    update tests for switch to package

--- test/functional/applique/fixtures.rb
+++ test/functional/applique/fixtures.rb
@@ -1,15 +1,16 @@
 FIXTURE_DIR = 'tmp/example/'
 
-When 'iven a (((\w+))) project file' do |name, text|
+When 'iven a ((([\.\w]+))) project file' do |name, text|
   FileUtils.mkdir_p(FIXTURE_DIR)
   File.open(FIXTURE_DIR + name, 'w') do |f|
     f << text
   end
 end
 
-After :document do
-  Dir[FIXTURE_DIR + '**/*'].each do |file|
-    FileUtils.rm_r(file) if File.exist?(file)
-  end
-end
+# TODO: remove the whole directory
+#After :document do
+#  Dir[FIXTURE_DIR + '**/*'].each do |file|
+#    FileUtils.rm_r(file) if File.exist?(file)
+#  end
+#end
 
