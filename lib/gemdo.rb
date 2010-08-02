1dee034653e1d9374b85cc4d3e9ca3272efe2011
7rans <transfire@gmail.com>
2010-08-02 15:38:24 -0400

    admin: tonight we dance, for .ruby is done

--- lib/gemdo.rb
+++ lib/gemdo.rb
@@ -1,19 +1,20 @@
-require 'gemdo/project'
-
 module Gemdo
   DIRECTORY = File.dirname(__FILE__)
 
-  #def self.metadata
-  #  @metadata ||= %w{version profile gemfile}.inject({}) do |data, name|
-  #    data.merge(YAML.load(File.new(DIRECTORY + "/rock.meta/#{name}.yml")))
-  #    data
-  #  end
-  #end
+  def self.metadata
+    @metadata ||= %w{PROFILE PACKAGE}.inject({}) do |data, name|
+      file = DIRECTORY + "/gemdo.ruby/#{name}"
+      data.merge!(YAML.load(File.new(file)))
+      data
+    end
+  end
 
   # Access to project metadata, e.g. VERSION.
-  #def self.const_missing(name)
-  #  name = name.to_s.downcase
-  #  metadata[name]
-  #end
+  def self.const_missing(name)
+    name = name.to_s.downcase
+    metadata[name]
+  end
 end
+
+require 'gemdo/project'
 
